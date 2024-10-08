55f1b8e1849581db966b5f122f8fadf8121c404c
Alvina004 <gabrielyanalvina2@gmail.com>
2024-10-08 14:23:32 +0400

    Update rowcol.h

--- rowcol.h
+++ rowcol.h
@@ -3,22 +3,18 @@
 #include "Crypto.h"
 #include <cstddef>
 
-const unsigned int TemplateSize = 256;
-
 class rowcol : public Crypto
 {
 public:
-	rowcol();
-	virtual ~rowcol();
-
-	virtual int Encrypt(const unsigned char* pSrc, unsigned int SrcSz, unsigned char* pOut, unsigned int& Outsz);
+    rowcol();          
+    virtual ~rowcol(); 
+    
+    virtual int Encrypt(const unsigned char* pSrc, unsigned int SrcSz, unsigned char* pOut, unsigned int& Outsz);
     virtual int Decrypt(const unsigned char* pSrc, unsigned int SrcSz, unsigned char* pOut, unsigned int& Outsz);
 
-	int SetKey(unsigned int);
-	int GetKey();
+    int SetKey(unsigned int sz);
+    int GetKey();
 
 private:
-
-	size_t mkey;
-
+    size_t mkey; 
 };
